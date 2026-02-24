155665f2f810cfdccf9621e4fec9244a11143eb8
pofe <paolo.forte@oru.se>
2026-02-24 15:50:32 +0100

    Added athena_common package.

--- execution/athena_exe_launch/launch/execution.py
+++ execution/athena_exe_launch/launch/execution.py
@@ -21,7 +21,7 @@ from launch.actions import DeclareLaunchArgument, GroupAction, SetEnvironmentVar
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import PushRosNamespace,Node
-from nav2_common.launch import RewrittenYaml
+from athena_common.launch import RewrittenYaml
 
 
 def generate_launch_description():
